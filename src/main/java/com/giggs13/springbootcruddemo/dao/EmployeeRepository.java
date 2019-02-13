5c10a9bacb871541686e3ce1b3c62f08ce54c3e3
andrejmalykhin <Andrej.Malykhin.13@gmail.com>
2019-02-13 21:59:55 +0200

    77. Spring Boot - Spring Data REST - Real-Time Project

--- src/main/java/com/giggs13/springbootcruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/giggs13/springbootcruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.giggs13.springbootcruddemo.dao;
 
 import com.giggs13.springbootcruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository
         extends JpaRepository<Employee, Integer> {
 
